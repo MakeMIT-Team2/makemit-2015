06e0063b78b4b36d3dde5f0cf41fa3e9aa11509a
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-03-01 16:47:40 -0500

    final project

--- serve/static/js/index.js
+++ serve/static/js/index.js
@@ -7,7 +7,7 @@ setTimeout(function(){
 	socket.on('d', function(d){
 		data_from_server.push(d);
 		console.log('Making cube!');
-		console.log(d.x * (50/10) * 80, (7.8 - d.y) * (50/10) * 40, d.z * (50/10) * 80, d.str)
-		makeCube((d.x * (50/10) * 80), (7.8 - d.y) * (50/10) * 40, d.z * (50/10) * 80, d.str);
+		console.log(d.x * (50/10) * 80, (7.8 - d.y) * (50/10) * 80, d.z * (50/10) * 80, d.str)
+		makeCube((d.x * (50/10) * 60), (7.8 - d.y) * (50/10) * 60, d.z * (50/10) * 60 - 700, d.str);
 	});
 }, 1000);
